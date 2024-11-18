c42bb56124786baef18296b2293a8a79d1bc6311
IngFabian97 <fabian.guarin@hotmail.com>
2024-11-18 17:44:56 -0500

    Creacion del display

--- app/src/main/java/calculadora/CalculadoraGUI.java
+++ app/src/main/java/calculadora/CalculadoraGUI.java
@@ -1,5 +1,8 @@
 package main.java.calculadora;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -15,6 +18,12 @@ public class CalculadoraGUI extends JFrame{
         setTitle("Calculadora");
         setSize(400,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        display = new JTextField();
+        display.setEditable(false);
+        display.setFont(new Font("Arial", Font.PLAIN, 50));
+        add(display,BorderLayout.NORTH);
+        
     }
 
 }
